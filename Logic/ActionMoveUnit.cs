4f1918fa05837eeafa9e25d99fa7b4ab4ee583bc
PencilAmazing <saifshw@live.com>
2022-11-18 14:51:35 -0500

    Add a dedicated turn event, and separate turning in ActionMoveUnit to it's own phase loop

--- Logic/ActionMoveUnit.cs
+++ Logic/ActionMoveUnit.cs
@@ -18,12 +18,17 @@ namespace TAC.Logic
 			path.unit.phase += 1;
 			if (path.unit.phase > 8) {
 				path.unit.phase = 0;
+
+				// Turning takes a whole phase loop
+				UnitDirection dir = Pathfinding.GetDirection(path.unit.position, path.path[0]);
+				if (path.unit.direction != dir) {
+					path.unit.direction = dir;
+					return;
+				}
+
 				// Advance unit
 				scene.floor[path.unit.position.x, path.unit.position.z].unit = null;
 
-				UnitDirection dir = Pathfinding.GetDirection(path.unit.position, path.path[0]);
-				path.unit.direction = dir;
-				
 				path.unit.position = path.path[0];
 				path.path.RemoveAt(0);
 				scene.floor[path.unit.position.x, path.unit.position.z].unit = path.unit;
---
+++ Logic/ActionTurnUnit.cs
@@ -0,0 +1,45 @@
+﻿using ImGuiNET;
+using System;
+using System.Reflection.Metadata;
+using TAC.World;
+
+namespace TAC.Logic
+{
+	/// <summary>
+	/// Turn around a unit. Searches for shortest distance to minimize turns
+	/// </summary>
+	public class ActionTurnUnit : Action
+	{
+		private Unit unit;
+		private UnitDirection targetDirection;
+
+		private int phase; // TODO move to base class
+
+		public ActionTurnUnit(Scene scene, Unit unit, UnitDirection targetDirection) : base(scene)
+		{
+			this.scene = scene;
+			this.unit = unit;
+			this.targetDirection = targetDirection;
+
+			this.phase = 0;
+		}
+
+		public override void Think(float deltaTime)
+		{
+			base.Think(deltaTime);
+
+			// https://math.stackexchange.com/a/2898118
+			// Distance of target enum from current ennum
+			int delta = ((int)targetDirection - (int)unit.direction + 12) % 8 - 4;
+
+			phase += 1;
+			if (phase > 8) {
+				if (delta == 0) Done();
+				phase = 0;
+				UnitDirection nextDirection = (UnitDirection)(((int)unit.direction + Math.Sign(delta)) % 8);
+				unit.direction = nextDirection;
+				delta -= 1;
+			}
+		}
+	}
+}
